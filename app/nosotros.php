27e1ed38de0b801429bed2f60874358ade47b3ca
Adriaya63 <Adriaya63@gmail.com>
2022-10-16 20:26:36 +0200

    Pagina informacion

--- app/nosotros.php
+++ app/nosotros.php
@@ -6,6 +6,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Servicio Web</title>
   <link rel="stylesheet" type="text/css" href="css/headerSets.css">
+  <link rel="stylesheet" type="text/css" href="css/nosotrosStyle.css">
 </head>
 <?php include("templates/header.php")?>
+<h1>¿Quienes somos?</h1>
+<br>
+<div class="info1">
+  <p>Somos un grupo de trabajo de la asignatura de Sistemas de Gestión de Seguridad de Sistemas de Información formado por Adrián Ayabarrena Muguruza y Sergio Gonzalez Ramos</p>
+</div><br>
+<h1>¿De que trata la pagina?</h1>
+<br>
+<div class="info2">
+  <p>Esta pagina es la simulacion de una empresa de alquiler de alojamientos llamada Homïes </p>
+</div>
+<br>
+<div class="sub"><h2>Dentro de esta pagina se puede:</h2>
+  <p>-Ver el listado de alojamientos <br>-Añadir y eliminar alojamientos <br>-Modificar la informacion de los alojamientos <br>-Registrar un usuario
+ <br>-Iniciar ssion con un usuario registrado <br>-Modificar los datos de un usuario</p>
+</div>
 <?php include("templates/footer.php")?>
